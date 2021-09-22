01e2e282364e907974d2927717a6b646d8072be6
arslanki5 <arsalan.khan@ki5.co.uk>
2021-09-22 15:30:09 +0500

    marketing api

--- src/api/extensions/mail-service/index.js
+++ src/api/extensions/mail-service/index.js
@@ -47,6 +47,41 @@ module.exports = ({ config }) => {
       apiStatus(res, 'That Some Error Occurred while sending contact us email', 500);
     }
   })
+  msApi.post('/update', async (req, res) => {
+    try {
+      try {
+        const customerUpdateDetails = req.body;
 
+        let url = config.extensions.marketing.endpoint + '/rest/default/V1/w10/marketing';
+        console.log('url', url);
+
+        const customerUpdateResponse = await axios.post(url,
+          customerUpdateDetails,
+          {
+            headers: {
+              'Content-type': 'application/json'
+            }
+          }
+        );
+        console.log(customerUpdateResponse);
+        apiStatus(res, customerUpdateResponse.data);
+
+      } catch (error) {
+        console.error(error);
+        apiStatus(
+          res,
+          {
+            message: 'Some Error Occurred while processing fetching customerUpdateResponse Url',
+            reqBody: req.body,
+            error
+          },
+          500
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      apiStatus(res, 'That Error Occurred while processing fetching customerUpdateResponse Url', 500);
+    }
+  });
   return msApi
 }
